c567782cd2f549adf29aee68fcfd1e4f089be14e
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-23 14:15:14 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='BGR',
+      version='0.1.0',
+      description='Python module for interacting with Chatbase APIs',
+      url='https://github.com/github-noob-arjun/BGRemover-bot',
+      author='Github',
+      author_email='None',
+      license='MIT Pro',
+      packages=['BGR'],      
+      zip_safe=False)
